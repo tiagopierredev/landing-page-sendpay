3680954522a161f571fb4ba674e6eda1f771dfb3
tiagopierre <tiago_pierre@outlook.com>
2022-04-14 13:28:55 -0300

    finished application

---
+++ index.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal(".reveal-bottom", {
+  delay: 500,
+  origin: "top",
+  distance: "50%",
+  reset: true,
+});
+
+ScrollReveal().reveal(".reveal-left", {
+  delay: 500,
+  origin: "left",
+  distance: "50%",
+  reset: true,
+});
+
+ScrollReveal().reveal(".reveal-right", {
+  delay: 500,
+  origin: "right",
+  distance: "50%",
+  reset: true,
+});
